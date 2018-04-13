a0d55b24f88419ea44e28979f8ea7f87130573c4
matsushita <is0280fp@ed.ritsumei.ac.jp>
2018-04-14 01:33:01 +0900

    データの幅を10000から1000にすることでlam_aの変化を抑えた

--- poissonglm.py
+++ poissonglm.py
@@ -12,18 +12,22 @@ import basic_distributions
 
 
 def gf(mu_a, mu_b, x):
-    i = np.arange(len(x))
+    i = np.arange(len(x))/1000
     lam = np.exp(mu_a*i + mu_b)
+#
+#    gy_a = np.sum((x/lam - 1) * lam*i)
+#    gy_b = np.sum((x/lam - 1) * lam)
 
-    gy_a = np.sum((x/lam - 1) * lam*i)
-    gy_b = np.sum((x/lam - 1) * lam)
+    gy_a = np.sum(x*i - lam*i)
+    gy_b = np.sum(x - lam)
     return gy_a, gy_b
 
 
 def f(mu_a, mu_b, x):
-    i = np.arange(len(x))
-    c = mu_a*i + mu_b
-    return np.sum(x*c - np.exp(c) - scipy.special.loggamma(x + 1).real)
+    i = np.arange(len(x))/1000
+    temp = mu_a*i + mu_b
+#    print("temp", temp)
+    return np.sum(x*temp - np.exp(temp) - scipy.special.loggamma(x + 1).real)
 
 
 if __name__ == '__main__':
@@ -37,23 +41,23 @@ if __name__ == '__main__':
     i = np.arange(n, dtype=np.int64)
 
     #    初期値
-    mean_a = 0.003
-    mean_b = 0.1
-    step_size = 0.0000000001
+    mean_a = 0.001 + 0.01
+    mean_b = 1 + 0.01/666
+    step_size = 0.0001
     mean_a_list = [mean_a]
     mean_b_list = [mean_b]
     gy_norm_list = []
-    mean_a_size = 5
-    mean_b_size = 10
+    mean_a_size = 30
+    mean_b_size = 30
     y_list = []
     Y_list = []
 
-    _mean_a = np.linspace(0.00025, 0.00065, mean_a_size)
-    _mean_b = np.linspace(0.005, 1.5, mean_b_size)
+    _mean_a = np.linspace(0.1, 3, mean_a_size)
+    _mean_b = np.linspace(0.5, 1.7, mean_b_size)
     grid_mean_a, grid_mean_b = np.meshgrid(_mean_a, _mean_b)
     for xxmean_a, xxmean_b in zip(grid_mean_a, grid_mean_b):
         for xmean_a, xmean_b in zip(xxmean_a, xxmean_b):
-            Y_list.append(f(mean_a, mean_b, x))
+            Y_list.append(f(xmean_a, xmean_b, x))
     Y = np.reshape(Y_list, (mean_b_size, mean_a_size))
 
     for i in range(1, 1000):
@@ -70,24 +74,25 @@ if __name__ == '__main__':
 
         plt.plot(mean_a_list, mean_b_list, ".-")
         plt.contour(grid_mean_a, grid_mean_b, Y)
-        plt.xlim(_mean_a[0], _mean_a[-1])
+#        plt.xlim(_mean_a[0], _mean_a[-1])
         plt.grid()
         plt.colorbar()
 #        plt.gca().set_aspect('equal')
         plt.show()
 
-        plt.plot(y_list)
-        plt.grid()
-        plt.show()
-
-        plt.plot(gy_norm_list)
-        plt.grid()
-        plt.show()
+#        plt.plot(y_list)
+#        plt.grid()
+#        plt.show()
+#
+#        plt.plot(gy_norm_list)
+#        plt.grid()
+#        plt.show()
 
         print("step_size", step_size)
-        print("gy_norm:", gy_norm)
         print(i, mean_a, mean_b)
         print("y:", y)
+        print("gy1, gy2", gy1, gy2)
+        print("gy_norm:", gy_norm)
 
 #    print("mean_a, mean_b", mean_a, mean_b)
 #    i = np.arange(len(x))
