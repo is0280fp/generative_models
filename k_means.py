1e22094d34c6b6fc67c20f4432ea5d785c16baa1
matsushita <is0280fp@ed.ritsumei.ac.jp>
2018-05-02 21:13:28 +0900

    indicasをindicesに変更。distance_dataメソッドをsklearnのpairwise_distancesメソッドに変更。初期値に対する１ステップ前のクラスタリング重心をnp.full_likeメソッドで書いた。古いコメントを新しくした。

--- k_means.py
+++ k_means.py
@@ -7,37 +7,28 @@ Created on Mon Apr 23 12:30:17 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import pairwise_distances
 from generate_clustering_data import generate_clustering_data
-
-
-def distance_data(X, centroids):
-    each_dis = []
-    for x in X:
-        each_dis.append(np.linalg.norm(x - centroids, axis=1))
-    return np.array(each_dis)
 
 
 if __name__ == '__main__':
     K = 4
     X = generate_clustering_data()
     N, D = X.shape
-    indicas = np.random.choice(N, K, replace=False)
+    indices = np.random.choice(N, K, replace=False)
     #    (K, D)
-    #  現在のクラスタリング重心
-    centroids = X[indicas]
+    #  クラスタリング重心の初期値
+    centroids = X[indices]
 
     #  クラスタリング
-    #  一回目のクラスタリング
-    each_dis = distance_data(X, centroids)
+    each_dis = pairwise_distances(X, centroids)
     clusters = each_dis.argmin(axis=1)
 
-    #  二回目以降のクラスタリング
-    j = 0
-    #  １ステップ前のクラスタリング重心
-    prev_centroids = np.zeros((K, D))
-    prev_centroids[:] = np.nan
+    #  初期値に対する１ステップ前のクラスタリング重心
+    prev_centroids = np.full_like(centroids, np.nan)
     #  1ステップ前と現在のクラスタリング重心を比較、同じであればクラスタリング終了
     while np.array_equal(centroids, prev_centroids) is False:
+        #  １ステップ前のクラスタリング重心
         prev_centroids = centroids
 
         #  各クラスタの重心が求める
@@ -48,11 +39,9 @@ if __name__ == '__main__':
         centroids = np.array(centroids_lst)
 
         #  重心を基にデータをクラスタリングしなおす
-        each_dis = distance_data(X, centroids)
+        each_dis = pairwise_distances(X, centroids)
         clusters = each_dis.argmin(axis=1)
 
-        #  ステップ数の更新
-        j += 1
         #  クラスタリング重心の表示
         print("prev centroids", prev_centroids)
         print("now centroids", centroids)
@@ -61,6 +50,7 @@ if __name__ == '__main__':
     #  クラスタリング結果描画
     for k in np.arange(K):
         plt.plot(X[clusters == k][:, 0], X[clusters == k][:, 1], ".")
+        plt.plot(centroids[0:, 0], centroids[0:, 1], "*")
     plt.xlabel("Dim1")
     plt.ylabel("Dim2")
     plt.show()
