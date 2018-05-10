0da7d96fac6e8d805db5853100045132b07f88be
matsushita <is0280fp@ed.ritsumei.ac.jp>
2018-05-10 16:16:11 +0900

    変数名を変えた

--- em_algorithm.py
+++ em_algorithm.py
@@ -11,10 +11,10 @@ from scipy import stats as st
 import mixture_distributions
 
 
-def likehood_function(x, mean_k, sigma_k, pi_k):
+def likehood_function(x, mean_k, var_k, pi_k):
     lkh_lst = []
-    for mean, sigma, pi in zip(mean_k, sigma_k, pi_k):
-        lkh_lst.append(np.exp(-(X-mean)**2/2*sigma) / (2*np.pi*sigma)**0.5)
+    for mean, var, pi in zip(mean_k, var_k, pi_k):
+        lkh_lst.append(np.exp(-(X-mean)**2/2*var) / (2*np.pi*var)**0.5)
     return np.array(lkh_lst, dtype=np.float64)
 
 
@@ -35,10 +35,10 @@ if __name__ == '__main__':
     N = X.shape[0]
     #    パラメタ初期値設定
     mean_k = np.arange(1, K+1)
-    sigma_k = np.arange(1, K+1)
+    var_k = np.arange(1, K+1)
     pi_k = np.random.dirichlet([1] * K)
     #    パラメタ初期値での対数尤度
-    lkh_k = likehood_function(X, mean_k, sigma_k, pi_k)
+    lkh_k = likehood_function(X, mean_k, var_k, pi_k)
     log_lkh = np.log(lkh_k.sum(axis=0)).sum()
     prev_log_lkh = np.log(lkh_k.sum(axis=0)).sum()
 
@@ -63,15 +63,15 @@ if __name__ == '__main__':
             mean_k_lst.append((ganma_lst[i] * X).sum() / N_k[i])
         mean_k = np.array(mean_k_lst)
 
-        sigma_k_lst = []
+        var_k_lst = []
         for i in np.arange(K):
-            sigma_k_lst.append((
+            var_k_lst.append((
                     ganma_lst[i] * (X - mean_k[i]) * (
                             X - mean_k[i]).transpose()).sum() / N_k[i])
-        sigma_k = np.array(sigma_k_lst)
+        var_k = np.array(var_k_lst)
 
         pi_k = N_k / N_k.sum()
-        lkh_k = likehood_function(X, mean_k, sigma_k, pi_k)
+        lkh_k = likehood_function(X, mean_k, var_k, pi_k)
 
         #  対数尤度の計算
         log_lkh = np.log(lkh_k.sum(axis=0)).sum()
@@ -80,11 +80,11 @@ if __name__ == '__main__':
         print("prev log-like-hood", prev_log_lkh)
         print("log-lkie-hood", log_lkh)
         print("mean_k", mean_k)
-        print("sigma_k", sigma_k)
+        print("var_k", var_k)
         print("pi_k", pi_k)
 
         #  各ガウス分布の描画
-        std_k = sigma_k ** 0.5
+        std_k = var_k ** 0.5
         for k in np.arange(K):
             data = np.random.normal(mean_k[k], std_k[k], N)
             hist(data)
