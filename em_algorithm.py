1f38daab5c0ba16f1916111945a2bd642c521213
matsushita <is0280fp@ed.ritsumei.ac.jp>
2018-05-15 21:04:01 +0900

    変数名を変えた

--- em_algorithm.py
+++ em_algorithm.py
@@ -17,32 +17,26 @@ def gaussian_pdf(x, mean, var):
     return np.exp(-(x-mean)**2/(2*var)) / (2*np.pi*var)**0.5
 
 
-def gamma(x, mean_k, var_k, pi_k):
+def gamma(x, mean, var, pi):
     """
     式(9.23)
     """
     lkh = []
-    for mean, var, pi in zip(mean_k, var_k, pi_k):
-        lkh.append(pi * gaussian_pdf(x, mean, var))
+    for mean_k, var_k, pi_k in zip(mean, var, pi):
+        lkh.append(pi_k * gaussian_pdf(x, mean_k, var_k))
     lkh = np.array(lkh)
     lkhs = np.sum(lkh, 0)
     return lkh/lkhs
 
 
-def loglikelihood(x, mean_k, var_k, pi_k):
+def loglikelihood(x, mean, var, pi):
     """
     式(9.28)
     """
-    lkh_lst = []
-    for mean, var, pi in zip(mean_k, var_k, pi_k):
-        lkh_lst.append(pi * gaussian_pdf(x, mean, var))
-    return np.sum(np.log(np.sum(lkh_lst, 0)))
-
-
-def hist(data):
-    x_min, x_max = int(min(data)), int(max(data)+1)
-    bins = np.arange(x_min, x_max, 0.2)
-    plt.hist(data, bins=bins, range=(x_min, x_max))
+    lkh = []
+    for mean_k, var_k, pi_k in zip(mean, var, pi):
+        lkh.append(pi_k * gaussian_pdf(x, mean_k, var_k))
+    return np.sum(np.log(np.sum(lkh, 0)))
 
 
 if __name__ == '__main__':
@@ -55,64 +49,65 @@ if __name__ == '__main__':
     K = 3
     N = X.shape[0]
     #    パラメタ初期値設定
-    mean_k = np.arange(1, K+1)
-    var_k = np.arange(1, K+1)
-    pi_k = np.random.dirichlet([1] * K)
+    mean = np.arange(1, K+1)
+    var = np.arange(1, K+1)
+    pi = np.random.dirichlet([1] * K)
     #    パラメタ初期値での対数尤度
     log_lkh_lst = []
-    log_lkh = loglikelihood(X, mean_k, var_k, pi_k)
+    log_lkh = loglikelihood(X, mean, var, pi)
     log_lkh_lst.append(log_lkh)
-    prev_log_lkh = loglikelihood(X, mean_k, var_k, pi_k)
+    prev_log_lkh = loglikelihood(X, mean, var, pi)
 
     while True:
-#        assert prev_log_lkh <= log_lkh
-        prev_log_lkh = loglikelihood(X, mean_k, var_k, pi_k)
+#        assert prev_log_lkh < log_lkh
+        prev_log_lkh = loglikelihood(X, mean, var, pi)
 
         #  Eステップ(負担率の計算)
-        gammas = gamma(X, mean_k, var_k, pi_k)
+        gammas = gamma(X, mean, var, pi)
 
         #  Mステップ(パラメタ値を再計算)
         #  式(9.27)
-        N_k = []
+        Ns = []
         for k in np.arange(K):
-            N_k.append(gammas[k].sum())
-        N_k = np.array(N_k)
+            Ns.append(gammas[k].sum())
+        Ns = np.array(Ns)
 
-        mean_k = []
+        mean = []
         #  式(9.24)
-        for gamma_k, n_k in zip(gammas, N_k):
-            mean_k.append((gamma_k * X).sum() / n_k)
-        mean_k = np.array(mean_k)
+        for gamma_k, N_k in zip(gammas, Ns):
+            mean.append((gamma_k * X).sum() / N_k)
+        mean = np.array(mean)
 
-        var_k = []
+        var = []
         #  式(9.25)
-        for gamma_k, n_k, mean in zip(gammas, N_k, mean_k):
-            var_k.append((
-                    gamma_k * (X - mean) * (X - mean).transpose()).sum() / n_k)
-        var_k = np.array(var_k)
+        for gamma_k, N_k, mean_k in zip(gammas, Ns, mean):
+            var.append((
+                    gamma_k * (X - mean_k) * (X - mean_k).transpose()
+                    ).sum() / N_k)
+        var = np.array(var)
 
         #  式(9.26)
-        pi_k = N_k / N_k.sum()
+        pi = Ns / Ns.sum()
 
         #  対数尤度の計算
-        log_lkh = loglikelihood(X, mean_k, var_k, pi_k)
+        log_lkh = loglikelihood(X, mean, var, pi)
         log_lkh_lst.append(log_lkh)
 
         #  標準偏差の計算
-        std_k = var_k ** 0.5
+        std = var ** 0.5
 
         #  対数尤度の出力
         print("prev log-likelihood", prev_log_lkh)
         print("log-lkielihood", log_lkh)
-        print("mean_k", mean_k)
-        print("var_k", var_k)
-        print("std_k", std_k)
-        print("pi_k", pi_k)
+        print("mean", mean)
+        print("var", var)
+        print("std", std)
+        print("pi", pi)
 
         #  各ガウス分布確率密度関数の描画
         x_scope = np.linspace(np.min(X), np.max(X), num=N)
         for k in np.arange(K):
-            plt.plot(x_scope, gaussian_pdf(x_scope, mean_k[k], var_k[k]))
+            plt.plot(x_scope, gaussian_pdf(x_scope, mean[k], var[k]))
         plt.ylim(0, 0.5)
         plt.title("pdfs")
         plt.show()
@@ -120,7 +115,7 @@ if __name__ == '__main__':
         #  混合ガウシアン分布の確率密度関数の描画
         pdfs = []
         for k in np.arange(K):
-            pdfs.append(gaussian_pdf(x_scope, mean_k[k], var_k[k]))
+            pdfs.append(gaussian_pdf(x_scope, mean[k], var[k]))
         pdfs = np.array(pdfs)
         plt.plot(x_scope, pdfs.sum(axis=0))
         plt.ylim(0, 0.5)
