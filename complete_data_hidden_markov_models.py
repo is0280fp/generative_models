19c7db364cd622ee0033432af9b8f517c0dca811
matsushita <is0280fp@ed.ritsumei.ac.jp>
2018-05-22 17:38:09 +0900

    隠れマルコフモデルの完全データをもとにパラメタ推定した

---
+++ complete_data_hidden_markov_models.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 14:24:45 2018
+
+@author: yume
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import hidden_markov_models
+
+
+def gaussian_paras(x):
+    mean = x.mean()
+    var = x.var()
+    std = x.std()
+    return (mean, var, std)
+
+
+if __name__ == '__main__':
+    sampler = hidden_markov_models.GaussianHMM()
+    z, x = sampler(10000, complete_data=True)
+    sampler.visualize(x)
+    print("Distribution: ", sampler.get_name())
+    print("Parameters: ", sampler.get_params())
+
+    #  パラメタ推定処理
+    K = len(np.unique(z))  # 3
+    N = len(z)
+    p_initial_state = [0.4, 0.3, 0.3]
+
+    print("K:", K)
+    print("p_initial_state:", p_initial_state)
+
+    count1_1 = 0
+    count1_2 = 0
+    count1_3 = 0
+    count2_1 = 0
+    count2_2 = 0
+    count2_3 = 0
+    count3_1 = 0
+    count3_2 = 0
+    count3_3 = 0
+    prev_z = z[:-1]
+    for i in np.arange(1, N):
+        if prev_z[i-1] == 1:
+            if z[i] == 1:
+                count1_1 += 1
+            elif z[i] == 2:
+                count1_2 += 1
+            else:
+                count1_3 += 1
+        elif prev_z[i-1] == 2:
+            if z[i] == 1:
+                count2_1 += 1
+            elif z[i] == 2:
+                count2_2 += 1
+            else:
+                count2_3 += 1
+        else:
+            if z[i] == 1:
+                count3_1 += 1
+            elif z[i] == 2:
+                count3_2 += 1
+            else:
+                count3_3 += 1
+
+    count1 = count1_1 + count1_2 + count1_3
+    count2 = count2_1 + count2_2 + count2_3
+    count3 = count3_1 + count3_2 + count3_3
+    transition_matrix = []
+    transition_matrix.append([count2_2/count2, count2_1/count2, count2_3/count2])
+    transition_matrix.append([count1_2/count1, count1_1/count1, count1_3/count1])
+    transition_matrix.append([count3_2/count3, count3_1/count3, count3_3/count3])
+
+    xs = []
+    for k in range(K):
+        x_zi = x[z == k]
+        xs.append(x_zi)
+
+    mean_lst = []
+    var_lst = []
+    std_lst = []
+
+    for k in range(K):
+        mean, var, std = gaussian_paras(np.array(xs[k], dtype=np.float64))
+        mean_lst.append(mean)
+        var_lst.append(var)
+        std_lst.append(std)
+    means = np.array(mean_lst)
+    stds = np.array(std_lst)
+
+#  以下、モデル生成処理
+    z_1 = np.random.choice(K, 1, p=p_initial_state)
+    x_1 = np.random.normal(means[z_1], stds[z_1], 1)
+    z_new = [z_1]
+    x_new = [x_1]
+    for i in np.arange(1, N):
+        z_new.append(np.random.choice(K, 1,
+                                      p=transition_matrix[int(z_new[-1])]))
+        x_new.append(np.random.normal(
+                means[int(z_new[-1])], stds[int(z_new[-1])], 1))
+
+    x_new = np.array(x_new)
+    print("transition_matrix:", transition_matrix)
+    print("mean:", mean_lst)
+    print("std:", std_lst)
+    sampler.visualize(x_new)
