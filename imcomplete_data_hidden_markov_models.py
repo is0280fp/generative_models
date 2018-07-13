f567178a645ee6ca3cdf43d212a4cf2a4493a280
matsushita yume <is0280fp@ed.ritsumei.ac.jp>
2018-07-13 21:56:47 +0900

    alpha_lstをなくした

--- imcomplete_data_hidden_markov_models.py
+++ imcomplete_data_hidden_markov_models.py
@@ -31,46 +31,41 @@ def gaussian_pdfs(x, mean, var):
     return np.array(lkh).transpose()
 
 
-def loglikelihoods(x, mean, var, A):
+def loglikelihoods(x, mean, var, pi):
     """
     式(9.28)
     """
-    #  返り値: スカラー
     lkh = []
-    for j in range(A.shape[0]):
-        for k in range(A.shape[1]):
-            lkh.append(A[j, k] * gaussian_pdf(x, mean[k], var[k]))
+    for mean_k, var_k, pi_k in zip(mean, var, pi):
+        lkh.append(pi_k * gaussian_pdf(x, mean_k, var_k))
     return np.sum(np.log(np.sum(lkh, 0)))
 
 
-def compute_alpha_hat(x, mean, var, init_alpha, A):
+def compute_alpha_hat(init_alpha, A, gaus_pdfs):
     #  返り値alpha: 長さN, K次元のarray
     #  返り値c: 長さN-1, 1次元のarray
-    gaus_pdf = gaussian_pdfs(x, mean, var)
-    alpha_lst = []
-    init_hat_alpha = init_alpha / init_alpha.sum()
-    alpha_hat_lst = [init_hat_alpha]
+    init_alpha_hat = init_alpha / init_alpha.sum()
+    alpha_hat_lst = [init_alpha_hat]
     c_lst = [init_alpha.sum()]
     J = A.shape[0]
     K = A.shape[1]
     for n in range(1, N):
+        sum_lst = []
         for k in range(K):
-            sum_lst = []
             for j in range(J):
                 sum_lst.append(np.array(alpha_hat_lst[-1])[j] * A[j, k])
-            alpha_lst.append(np.array(sum_lst).sum())
-        alpha_lst[-K::] = alpha_lst[-K::] * gaus_pdf[n]
-        cn = np.array(alpha_lst[-K::]).sum()
-        alpha_hat_lst.append(alpha_lst[-K::]/cn)
+        alpha = np.array(sum_lst).reshape(K, K).sum(1) * gaus_pdfs[n]
+        cn = alpha.sum()
+        alpha_hat_lst.append(alpha/cn)
         c_lst.append(cn)
     return np.array(alpha_hat_lst), np.array(c_lst)
 
 
-def compute_beta_hat(x, mean, var, init_beta, A, c):
+def compute_beta_hat(init_beta, A, c, gaus_pdfs):
     #  返り値: 長さN, K次元のarray
-    gaus_pdf = gaussian_pdfs(x, mean, var)
     beta_lst = []
-    beta_hat_lst = [init_beta]
+    init_beta_hat = init_beta / c[-1]
+    beta_hat_lst = [init_beta_hat]
     J = A.shape[0]
     K = A.shape[1]
     for n in range(N-1)[::-1]:
@@ -78,24 +73,25 @@ def compute_beta_hat(x, mean, var, init_beta, A, c):
             sum_lst = []
             for k in range(K):
                 sum_lst.append(np.array(
-                        beta_hat_lst[-1])[k] * A[j, k] * gaus_pdf[n, k])
+                        beta_hat_lst[-1])[k] * A[j, k] * gaus_pdfs[n, k])
             beta_lst.append(np.array(sum_lst).sum())
         beta_hat_lst.append(beta_lst[-K::]/c[n])
     return np.array(beta_hat_lst)[::-1]
 
 
-def xi(x, mean, var, A, alpha, beta, gaus_pdf, c):
+def xi(A, alpha, beta, gaus_pdf, c):
     #  返り値: K*Kのarray, 長さN-1
     xi_lst = []
-    a = alpha[:-1]
-    b = beta[1:]
+    alpha = alpha[:-1]
+    beta = beta[1:]
+    c = c[1:]
     J = A.shape[0]
     K = A.shape[1]
     for n in range(N-1):
         for j in range(J):
             for k in range(K):
                 xi_lst.append(
-                        a[n, j] * gaus_pdf[n, k] * A[j, k] * b[n, k] * 1/c[n])
+                        alpha[n, j] * gaus_pdf[n, k] * A[j, k] * beta[n, k] * 1/c[n])
     return np.array(xi_lst).reshape(-1, K, K)
 
 
@@ -111,35 +107,35 @@ if __name__ == '__main__':
     N = X.shape[0]
 
     #    パラメタ初期値設定
+    pi = np.array([0.4, 0.3, 0.3])
     #  式(13.18)
-    A = np.array([[0.4, 0.3, 0.3],
-                  [0.4, 0.3, 0.3],
-                  [0.4, 0.3, 0.3]
+    A = np.array([[0.88 , 0.111, 0.009],
+                  [0.04 , 0.9  , 0.06 ],
+                  [0.002, 0.098, 0.9 ]
                   ])
     mean = np.arange(1, K+1)
     var = np.arange(1, K+1)
 
     #    パラメタ初期値での対数尤度
     log_lkh_lst = []
-    log_lkh = loglikelihoods(X, mean, var, A)
+    log_lkh = loglikelihoods(X, mean, var, pi)
     log_lkh_lst.append(log_lkh)
     prev_log_lkh = - np.inf
     init_beta = np.ones(K)
     gaus_pdf = gaussian_pdfs(X, mean, var)
-    pi = np.array([0.4, 0.3, 0.3])
 
     for iteration in np.arange(max_iter):
 #        assert prev_log_lkh < log_lkh
-        prev_log_lkh = loglikelihoods(X, mean, var, A)
+        prev_log_lkh = loglikelihoods(X, mean, var, pi)
         init_alpha = gaussian_pdfs(X[0], mean, var) * pi
 
         #  Eステップ(負担率の計算)
         #  gaus_pdf = p(Xn|Zn), shape(N, K)のarray
-        gaus_pdf = gaussian_pdfs(X, mean, var)
-        alpha_hat, c = compute_alpha_hat(X, mean, var, init_alpha, A)
-        beta_hat = compute_beta_hat(X, mean, var, init_beta, A, c)
+        gaus_pdfs = gaussian_pdfs(X, mean, var)
+        alpha_hat, c = compute_alpha_hat(init_alpha, A, gaus_pdfs)
+        beta_hat = compute_beta_hat(init_beta, A, c, gaus_pdfs)
         gammas = alpha_hat * beta_hat
-        xis = xi(X, mean, var, A, alpha_hat, beta_hat, gaus_pdf, c)
+        xis = xi(A, alpha_hat, beta_hat, gaus_pdf, c)
 
         #  Mステップ(パラメタ値を再計算)
         #  式(9.27), スカラー
@@ -163,13 +159,13 @@ if __name__ == '__main__':
         A = xis.sum(0) / xis.sum()
 
         #  対数尤度の計算
-        log_lkh = loglikelihoods(X, mean, var, A)
+        log_lkh = loglikelihoods(X, mean, var, pi)
         log_lkh_lst.append(log_lkh)
 
         #  収束判定
-        diff = log_lkh - prev_log_lkh
-        if diff < tol:
-            break
+#        diff = log_lkh - prev_log_lkh
+#        if diff < tol:
+#            break
 
         #  標準偏差の計算
         std = var ** 0.5
@@ -205,11 +201,11 @@ if __name__ == '__main__':
 
         #  対数尤度のグラフ
         plt.plot(np.array(log_lkh_lst))
-        plt.ylim(-45000, -35000)
+        plt.ylim(-58000, -35000)
         plt.title("log-likelihood")
         plt.grid()
         plt.show()
-        print("diff", diff)
+#        print("diff", diff)
         print("-----------------------------------------------------------------------")
 
     print("real data")
